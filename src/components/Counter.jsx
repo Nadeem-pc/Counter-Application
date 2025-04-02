b980fc5fe33f9aa7ca934480204ac6b39a4d4a93
Nadeem-pc <nadeempc777@gmail.com>
2025-04-02 13:22:52 +0530

    Implemented reset button and made some modifications

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,27 +1,34 @@
 import React, { useState } from 'react'
-import { buttons, counter } from './Counter.module.css'
+import { buttons, counter, countTxt, header } from './Counter.module.css'
 
 
 function Counter() {
     const [count, setCount] = useState(0);
 
-    function increment() {
+    const increment = () => {
         setCount(count + 1)
-    }
+    }   
 
-    function decrement() {
+    const decrement = () => {
         if(count === 0) return
         else  setCount(count - 1)
     }
 
+    const resetCounter = () => {
+        setCount(0)
+    }
+
     return (
-        <div className= { counter }>
-            <button  className= { buttons } onClick={ decrement }> <img src="/counter/src/assets/add_60dp_FFFFFF_FILL0_wght400_GRAD0_opsz48.svg" alt="" /> </button>
-
-            <h1> { count } </h1>
-
-            <button className= { buttons } onClick={ increment }> <span class="material-symbols-outlined"> add</span> </button>
-        </div>
+        <>
+            <div className={ header }>
+                <button onClick={resetCounter}> <span className="material-symbols-outlined" style={{ fontSize: "35px" }}>refresh</span> </button>
+            </div>
+            <div className= { counter }>
+                <button  className= { buttons } onClick={ decrement }> <span className="material-symbols-outlined">remove</span> </button>
+                <h1 className={ countTxt }>{ count }</h1>
+                <button className= { buttons } onClick={ increment }>   <span className="material-symbols-outlined">add</span> </button>
+            </div>
+        </>
     )
 }
 
